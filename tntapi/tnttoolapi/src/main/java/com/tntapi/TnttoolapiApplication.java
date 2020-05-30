c61cac4a6e2459dd7c1b93565234dc9a2a2892b9
theprateekdubey <40597635+theprateekdubey@users.noreply.github.com>
2020-05-30 21:48:11 +0530

    first commit for architecture

---
+++ tntapi/tnttoolapi/src/main/java/com/tntapi/TnttoolapiApplication.java
@@ -0,0 +1,13 @@
+package com.tntapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnttoolapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TnttoolapiApplication.class, args);
+	}
+
+}
---
+++ tntapi/tnttoolapi/src/test/java/com/tntapi/TnttoolapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tntapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TnttoolapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
